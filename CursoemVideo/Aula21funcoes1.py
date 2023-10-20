051058c9e764f068958a29d7d112db9b2a5fd51a
MateusSsL <147095321+MateusSsL@users.noreply.github.com>
2023-10-20 16:17:49 -0300

    update aulas 20/10/2023

---
+++ CursoemVideo/Aula21funcoes1.py
@@ -0,0 +1,21 @@
+# from cev import mensagem,soma
+
+# mensagem(titulo=' Funções ')
+
+# num=[4,8,9,2]
+# soma(*num)
+
+def area(a,b):
+    ca = a*b
+    print(f'Area : {ca:.2f} m²')
+
+a=float(input(f'Digite a Base do objeto: '))
+b=float(input(f'Digite a Altura do objeto: '))
+area(a,b)
+
+#https://youtu.be/ezfr9d7wd_k?si=NSEldZTJR1VvbVJN
+
+
+
+
+
---
+++ CursoemVideo/cev.py
@@ -0,0 +1,34 @@
+def mensagem(titulo):
+    print("-="*20)
+    print("{:-^40}".format(titulo))
+    print("-="*20)
+
+def lin():
+    print("-="*20)
+
+def centra(msg):
+    print("{:-^40}".format(msg))
+
+def contador(*num): #criar uma tupla com valores
+    print(num) # num(1, 2, 3, 4, 5)
+    # [1, 2, 3, 4, 5]
+
+def dobra(lst):
+    pos=0
+    while pos<len(lst):
+        lst[pos] *= 2
+        pos+=1
+        # valores = [2,4,3,5,6] criar lista
+        # print(valores)
+        # dobra(valores)
+        # print(valores)
+
+def soma(* valores):  #crie um lista para colocar em valores
+    s=0  #s vai receber a soma dos itens dentro da lista
+    for n in valores:  #num=[4,8,9,2]
+        s +=n
+    print(f"Somando os {valores} temos {s}")
+    #soma(*num)
+    
+
+
